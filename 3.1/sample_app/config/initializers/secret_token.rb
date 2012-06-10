3d0df3429f2b0960c85cc0fafeaf5827b80d9432
Joshua Ziggas <zoshjiggas@gmail.com>
2012-06-09 20:50:17 -0400

    Lesson 3.1: Modified the gemfile and added rspec for testing

---
+++ 3.1/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '88eaa5f2f4cfd7d5472abcf7fb8b9b6ade38714bac90d9e5c49fbb7951879ad4b6a7d566beda59a5ec27e28e9c3473658a479d29183a3fe2e04a93f72cdd9bca'
