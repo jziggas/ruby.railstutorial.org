f996f5c937f9e7b29ae2c7d277a7764faa2723be
Joshua Ziggas <zoshjiggas@gmail.com>
2012-06-26 14:20:13 -0400

    Fixed rspec testing bugs from ch5. Created session controller and test for authentication pages, beginning chapter 8.

--- 3.1/sample_app/spec/requests/static_pages_spec.rb
+++ 3.1/sample_app/spec/requests/static_pages_spec.rb
@@ -35,29 +35,29 @@ describe "Static pages" do
 
   describe "Help page" do
     before { visit help_path }
-    let(:heading)    { 'Sample App' }
+    let(:heading)    { 'Help' }
     let(:page_title) { 'Help' }
 
     it_should_behave_like "all static pages"
-    it { should have_selector 'title', text: '| Help' }
+
   end
 
     describe "About page" do
-      before { visit help_path }
-      let(:heading)    { 'Sample App' }
-      let(:page_title) { 'About' }
+      before { visit about_path }
+      let(:heading)    { 'About Us' }
+      let(:page_title) { 'About Us' }
 
       it_should_behave_like "all static pages"
-      it { should have_selector 'title', text: '| About' }
+
     end
 
 
   describe "Contact page" do
-    before { visit help_path }
-    let(:heading)    { 'Sample App' }
+    before { visit contact_path }
+    let(:heading)    { 'Contact' }
     let(:page_title) { 'Contact' }
 
     it_should_behave_like "all static pages"
-    it { should have_selector 'title', text: '| Contact' }
+
   end
 end
