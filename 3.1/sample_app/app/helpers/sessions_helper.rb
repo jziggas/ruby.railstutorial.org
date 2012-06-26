584802ea23539ee31b04a3a7e6460416c5356016
Joshua Ziggas <zoshjiggas@gmail.com>
2012-06-26 18:24:56 -0400

    complete up to 8.2.4; adding signin functionalty

--- 3.1/sample_app/app/helpers/sessions_helper.rb
+++ 3.1/sample_app/app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    self.current_user = user
+  end
+
+    def current_user=(user)
+      @current_user = user
+    end
+
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+    # "or equals" operator -- Its effect is to set the @current_user instance variable
+    # to the user corresponding to the remember token, but only if @current_user is undefined
+  end
 end
--- 3.1/sample_app/app/models/user.rb
+++ 3.1/sample_app/app/models/user.rb
@@ -13,12 +13,23 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-  before_save { self.email.downcase! }
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
+      # Because of the way Active Record synthesizes attributes based on database columns, without self
+      # the assignment would create a local variable called remember_token, which isn’t what we want at
+      # all. Using self ensures that assignment sets the user’s remember_token so that it will be written
+      # to the database along with the other attributes when the user is saved.
 
 end
