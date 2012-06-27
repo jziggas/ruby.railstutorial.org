518aa4257b142dde8474e6c3fd99ccd7ef58676f
Joshua Ziggas <zoshjiggas@gmail.com>
2012-06-27 16:24:05 -0400

    Chapter 8 complete. Skipped the exercises. User signin/signout and authentication.

--- 3.1/sample_app/app/helpers/sessions_helper.rb
+++ 3.1/sample_app/app/helpers/sessions_helper.rb
@@ -4,13 +4,22 @@ module SessionsHelper
     self.current_user = user
   end
 
-    def current_user=(user)
-      @current_user = user
-    end
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
 
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
     # "or equals" operator -- Its effect is to set the @current_user instance variable
     # to the user corresponding to the remember token, but only if @current_user is undefined
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 end
